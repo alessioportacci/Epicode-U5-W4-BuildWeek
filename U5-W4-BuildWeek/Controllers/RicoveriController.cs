5481fbf18f2978b931f46566bef4da3cc7c58960
Alessio <alessio.portacci.s@gmail.com>
2023-10-16 14:46:40 +0200

    Filtro ricoveri work in progress

---
+++ U5-W4-BuildWeek/Controllers/RicoveriController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using U5_W4_BuildWeek.Models.DbModels;
+
+namespace U5_W4_BuildWeek.Controllers
+{
+    public class RicoveriController : Controller
+    {
+
+        ModelDbContext db = new ModelDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult RicoveriFilter(List<int> Tipologie)
+        {
+            List<Animali> animali = db.Animali.Where(a => a.DataInizioRicovero != null && Tipologie.Contains(a.FkTipologia)).ToList();
+            return Json(animali);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
